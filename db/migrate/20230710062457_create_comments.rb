04ef1e60657dfcb7081a77b57f47d402ba200cf5
citec-47 <mauricendonyi40@gmail.com>
2023-07-11 10:03:43 -0700

    Fix linters

--- db/migrate/20230710062457_create_comments.rb
+++ db/migrate/20230710062457_create_comments.rb
@@ -1,10 +1,8 @@
-# frozen_string_literal: true
-
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.text :text
-      t.references :authors, null: false, foreign_key: { to_table: :users }
+      t.references :authors, null: false, foreign_key: {to_table: :users}
       t.references :posts, null: false, foreign_key: true
 
       t.timestamps
